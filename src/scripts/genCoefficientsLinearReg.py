580043b82628e7c16b8b1803fc40110c615003e7
prithm <pritspido@gmail.com>
2015-09-04 18:55:37 +0530

    Updated file for Linear Regression

--- src/scripts/genCoefficientsLinearReg.py
+++ src/scripts/genCoefficientsLinearReg.py
@@ -1,59 +1,66 @@
-print(__doc__)
-
-
-# Code source: Jaques Grobler
-# License: BSD 3 clause
-
-
-import matplotlib.pyplot as plt
+from sklearn import linear_model
 import numpy as np
-from sklearn import datasets, linear_model
+import sys
 
-# Load the diabetes dataset
-diabetes = datasets.load_diabetes()
+def main():
+
+	UserTweetLinks = {}
+	UserReTweetLinks = {}
+	tweetFeatures = eval(open(sys.argv[1], 'r').read().strip('\n')):
+
+	with open(sys.argv[1], 'r') as f:
+		for line in f:
+			myDict = eval(line.strip('\n'))
+			for key,value in myDict.items():
+				UserTweetLinks[key] = set(value)
 
 
-# Use only one feature
-diabetes_X = diabetes.data[:, np.newaxis]
-diabetes_X_temp = diabetes_X[:, :, 2]
+	with open(sys.argv[2], 'r') as f:
+		for line in f:
+			myDict = eval(line.strip('\n'))
+			for key,value in myDict.items():
+				UserReTweetLinks[key] = set(value)
 
-# Split the data into training/testing sets
-diabetes_X_train = diabetes_X_temp[:-20]
-diabetes_X_test = diabetes_X_temp[-20:]
 
-# Split the targets into training/testing sets
-diabetes_y_train = diabetes.target[:-20]
-diabetes_y_test = diabetes.target[-20:]
+	outfile = open(sys.argv[3], 'w')
 
-# Create linear regression object
-regr = linear_model.LinearRegression()
+	for user_id, tweetIDs in UserTweetLinks.items():
+		if user_id in UserReTweetLinks.keys():
+			tweetIdNotRetweeted = tweetIDs - UserReTweetLinks[user_id]
+			tweetVec = []
+			tweetTarget = []
+			for tweetId in tweetIdNotRetweeted:
+				if str(tweetId) in tweetFeatures.keys():
+					tweetVec.append(tweetFeatures[str(tweetId)])
+					tweetTarget.append(0)
+			if len(tweetVec) > 0:
+				continue
+			tweetReVec = []
+			tweetReTarget = []
+			for tweetId in UserReTweetLinks[user_id]:
+				if str(tweetId) in tweetFeatures.keys():
+					tweetReVec.append(tweetFeatures[str(tweetId)])
+					tweetReTarget.append(1)
+			if len(tweetReVec) > 0:
+				continue
+				
+			tweetVec += tweetReVec
+			tweetTarget += tweetReTarget
+					
+			regr = linear_model.LinearRegression()
+			regr.fit(a, b)
+			tempDict = {}
+			tempDict[user_id] = regr.coef_
+			outfile.write(str(tempDict)+'\n')
 
-# Train the model using the training sets
-regr.fit(diabetes_X_train, diabetes_y_train)
+	outfile.close()
 
-# The coefficients
-print('Coefficients: \n', regr.coef_)
-# The mean square error
-print("Residual sum of squares: %.2f"
-      % np.mean((regr.predict(diabetes_X_test) - diabetes_y_test) ** 2))
-# Explained variance score: 1 is perfect prediction
-print('Variance score: %.2f' % regr.score(diabetes_X_test, diabetes_y_test))
 
-# Plot outputs
-plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
-plt.plot(diabetes_X_test, regr.predict(diabetes_X_test), color='blue',
-         linewidth=3)
-
-plt.xticks(())
-plt.yticks(())
-
-plt.show()
-
+if __name__ == '__main__':
+	main()
 
 # a = [[1,2],[2,1]]
 # b = [1,2]
-# from sklearn import linear_model
-# import numpy as np
 # regr = linear_model.LinearRegression()
 # regr.fit(a, b)
 # print('Coefficients: \n', regr.coef_)
