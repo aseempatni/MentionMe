8f2c5390aee536dad2e70b7e66e3a212f20fce03
Aseem Patni <apatnip@gmail.com>
2015-09-23 00:32:28 +0530

    Added a module for performance evaluation with work flow The idea is to basically divide the data into test and train. Then learn from train and predict on test then measure performance on test data and report the same. #13

---
+++ src/evaluate/evaluate.py
@@ -0,0 +1,47 @@
+# We need to measure the performance of our model
+# For that, we'll use some evaluation techniques
+
+def divide_data(data):
+    # divide data into k+1 components
+    # out of these k will be used to test and
+    # the remaining 1 segment will be used for evaluation
+    k_segment = []
+    1_segment = []
+    return (k_segment, 1_segment)
+
+def train(data):
+    # train our model using the data
+    model = {}
+    return model
+
+def predict(model, test_data):
+    # given a learned model predict the values for the test_data
+    prediction = []
+    return prediction
+
+def evaluae(predicted_outcome, actual_outcome):
+    error = 0
+    for i in len(predicted_outcome):
+        predicted_value = predicted_outcome[i]
+        expected_value = actual_outcome[i]
+        error+=distance(predicted_value,actual_value)
+    performance = performance_from_error(error)
+    return performance
+
+def performance_from_error(error)
+    # convert error value to performance
+    # for example
+    performance = 1/error
+    return performance
+
+def distance(predicted, actual):
+    # measure how accurately are we predicting
+    distance = predicted - actual
+    return distance
+
+def main(data):
+    training_data, test_data = divide_data(data)
+    model = train(training_data)
+    prediction = predict(model,test_data)
+    performance = evaluate(prediction,truth)
+    print performance
