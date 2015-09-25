5fda13ed176700504032cc444c5bcd509b296595
Aseem Patni <apatnip@gmail.com>
2015-09-25 16:42:38 +0530

    linked the server with the recommendation system

--- src/server/recommendation.py
+++ src/server/recommendation.py
@@ -1,6 +1,13 @@
 # This is the file server imports and calles the method get to serve requests
 
+import sys
+import os
+sys.path.append(os.path.abspath('../scripts'))
+from recommend_mention_v2 import *
+
 def get(tweet):
     # Get recommendations for mention given a tweet
     # Return a list of users
+    reco = recommend_mention(tweet)
+    return reco
     return ['PatniAseem','agnivo_saha']
