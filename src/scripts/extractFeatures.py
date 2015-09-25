b1ceca91cd5b61651f4d681f4847a899e837356e
Agnivo <agnivo.saha@gmail.com>
2015-09-25 15:34:02 +0530

    Updated feature extraction and mention recommendation

--- src/scripts/extractFeatures.py
+++ src/scripts/extractFeatures.py
@@ -4,23 +4,23 @@ import math
 
 def getFriends(friendListFile) :
 	friends = {}
-	all_users = set()
+	# all_users = set()
 	with open(friendListFile, 'r') as f:
 		for line in f:
 			try :
 				currfriends = eval(line)
 				for currfriend in currfriends :
 					friends[str(currfriend)] = currfriends[currfriend]
-				for currfriend in currfriends :
-					for friend in currfriends[currfriend] :
-						all_users.add(str(friend))
+				# for currfriend in currfriends :
+				# 	for friend in currfriends[currfriend] :
+				# 		all_users.add(str(friend))
 			except Exception as e :
 				print line, e
-	return (friends, all_users)
+	return friends
 
 def generateUserTweetMapping(cleanTweets, tweetFile) :
 	users = {}
-	all_users = set()
+	# all_users = set()
 	with open(tweetFile, 'r') as f:
 		for line in f:
 			try:
@@ -29,49 +29,160 @@ def generateUserTweetMapping(cleanTweets, tweetFile) :
 					continue
 				if mydict['user']['id'] not in users :
 					users[str(mydict['user']['id'])] = []
-					all_users.add(str(mydict['user']['id']))
+					# all_users.add(str(mydict['user']['id']))
 					users[str(mydict['user']['id'])].append(str(mydict['id']))
 				else :
 					users[str(mydict['user']['id'])].append(str(mydict['id']))
 			except Exception as e :
 				print e
-	return (users, all_users)
+	return users
 
-def getTweets() :
-	 with open(tweetFile, 'r') as f:
-                for line in f:
-                        try:
-                            	mydict = eval(line)
-                                if str(mydict['id']) not in cleanTweets :
-                                        continue
-                                tweets[str(mydict['id'])] = {}
-                                tweets[str(mydict['id'])]['id'] = mydict['id']
-                                tweets[str(mydict['id'])]['text'] = mydict['text']
-                                tweets[str(mydict['id'])]['user'] = mydict['user']['id']
-                                tweets[str(mydict['id'])]['timestamp'] = datetime.datetime.strptime(str(mydict['created_at']), '%a %b %d %H:%M:%S +0000 %Y')
-                        except Exception as e :
-                                print e
+def getCleanTweets(cleanTweetFile) :
+	cleanTweets = set()
+	in_clean_tweets_file = open(cleanTweetFile, 'r')
+	dataCleanTweets = in_clean_tweets_file.read()
+	lines = dataCleanTweets.split('\n')
+	for line in lines :
+		words = line.split(' ')
+		cleanTweets.add(str(words[0]))
+	print "Num Clean Tweets : ", len(cleanTweets)
+	return cleanTweets
 
-def getTweetScores() :
+def getTweets(cleanTweets, tweetFile) :
+	tweets = {}
+	with open(tweetFile, 'r') as f:
+		for line in f:
+			try:
+				mydict = eval(line)
+				if str(50801670845771776) == str(mydict['id']) :
+                			print "Tweet exists before"
+				if str(mydict['id']) not in cleanTweets :
+					continue
+				tweets[str(mydict['id'])] = {}
+				tweets[str(mydict['id'])]['id'] = mydict['id']
+				tweets[str(mydict['id'])]['text'] = mydict['text']
+				tweets[str(mydict['id'])]['user'] = mydict['user']['id']
+				tweets[str(mydict['id'])]['timestamp'] = datetime.datetime.strptime(str(mydict['created_at']), '%a %b %d %H:%M:%S +0000 %Y')
+			except Exception as e :
+				print e
+	#if str(50801670845771776) in tweets :
+	#	print "Tweet exists after"
+	return tweets
+
+def getTweetScores(tweetDocTopicFile) :
+	lda_in = open(tweetDocTopicFile, 'r')
+	data = lda_in.read()
+	lines = data.split('\n')
 	tweetTopicScores = {}
-        for line in lines :
-                terms = line.split(' ')
-                i = 0
-                tweetId = ''
-                for term in terms :
-                        if i == 0 :
-                                tweetTopicScores[str(term)] = []
-                                tweetId = str(term)
-                        else :
-                              	tweetTopicScores[tweetId].append(float(term))
-                        i += 1
+	for line in lines :
+		terms = line.split(' ')
+		i = 0
+		tweetId = ''
+		for term in terms :
+			if i == 0 :
+				tweetTopicScores[str(term)] = []
+				tweetId = str(term)
+				if tweetId == str(48537243714977792) :
+					print "Num Topics for 48537243714977792 : ", len(terms)
+			else :
+				tweetTopicScores[tweetId].append(float(term))
+			i += 1
+	#if str(48537243714977792) in tweetTopicScores :
+	#	print "Exists here", len(tweetTopicScores[str(48537243714977792)])
+	return tweetTopicScores
 
-def getFeatures(tweetFile, friendListFile, w_score, cleenTweetFile, tweetDocTopicFile, UserTweetLinks, UserRetweetLinks) :
+def getUserTweetFeatures(UserTweets, user, tweetTopicScores, tweets, users, weight_factor, user_friends) :
+	
+	features = {}
+	weight_factor = float(weight_factor)
+	for tweet in UserTweets :
+		features[tweet] = []
+		#features[tweet].append('1') #for Bias term of an user who tweeted the tweet
+
+	prev = 0
+	j = 0
+	#print "Num tweets : ", len(tweets), " Num TweetTopicScores : ", len(tweetTopicScores)
+	try :	
+		for curr_tweet in UserTweets :
+			if str(curr_tweet) not in tweets :
+				print "Tweet Id not present : ", curr_tweet
+				continue
+			#print "Num Topic scores : ", len(tweetTopicScores[str(curr_tweet)]), curr_tweet
+			for topic in range(len(tweetTopicScores[str(curr_tweet)])):
+				#features for a topic of the tweet for an user
+				feature = 0
+				#print "Num user tweets : ", len(users[str(user)])
+				for tweet in users[str(user)] :
+					time_factor = math.exp(weight_factor * float(abs((tweets[tweet]['timestamp'] - tweets[str(curr_tweet)]['timestamp']).total_seconds())))
+					feature += (tweetTopicScores[tweet][topic] * tweetTopicScores[str(curr_tweet)][topic] * time_factor)
+				features[curr_tweet].append(feature)
+
+			#features for a topic of the tweet for a friend of this user
+			num_friends = 0
+			for friend in user_friends[str(user)] :
+				for topic in range(len(tweetTopicScores[str(curr_tweet)])) :
+					feature = 0
+					if str(friend) not in users :
+						#print 'No tweets found for', str(friend)
+						continue
+					num_friends += 1
+					for tweet in users[str(friend)] :
+						time_factor = math.exp(weight_factor * float(abs((tweets[tweet]['timestamp'] - tweets[str(curr_tweet)]['timestamp']).total_seconds())))
+						feature += (tweetTopicScores[str(tweet)][topic] * tweetTopicScores[str(curr_tweet)][topic] * time_factor)
+					features[curr_tweet].append(feature)
+			if len(features[curr_tweet]) != ((num_friends + 100)) :
+				print "Error : Num Friends : ", num_friends, " Num Features : ", len(features[curr_tweet])," Tweet Id : ", curr_tweet
+			if j == 0 :
+				prev = len(features[curr_tweet])
+			else :
+				curr = len(features[curr_tweet])
+				if curr != prev :
+					print "Error in Feature Extraction : Curr : ", curr, ", Prev : ", prev
+			j += 1
+	except Exception as e :
+		print e
+	return features
+
+def getTweetFeatures(user, tweetTopicScores, tweets, users, weight_factor, user_friends, tweetDistributions) :
+	features = []
+	weight_factor = float(weight_factor)
+
+	try :	
+		for topic in range(len(tweetDistributions)):
+			#features for a topic of the tweet for an user
+			feature = 0
+			#print "Num user tweets : ", len(users[str(user)])
+			for tweet in users[str(user)] :
+				time_factor = math.exp(weight_factor * float(abs((tweets[tweet]['timestamp'] - datetime.datetime.now).total_seconds())))
+				feature += (tweetTopicScores[tweet][topic] * tweetDistributions[topic] * time_factor)
+			features.append(feature)
+
+			#features for a topic of the tweet for a friend of this user
+			num_friends = 0
+			for friend in user_friends[str(user)] :
+				for topic in range(len(tweetTopicScores[str(curr_tweet)])) :
+					feature = 0
+					if str(friend) not in users :
+						#print 'No tweets found for', str(friend)
+						continue
+					num_friends += 1
+					for tweet in users[str(friend)] :
+						time_factor = math.exp(weight_factor * float(abs((tweets[tweet]['timestamp'] - datetime.datetime.now).total_seconds())))
+						feature += (tweetTopicScores[str(tweet)][topic] * tweetDistributions[topic] * time_factor)
+					features.append(feature)
+			if len(features) != ((num_friends + 100)) :
+				print "Error : Num Friends : ", num_friends, " Num Features : ", len(features)," Tweet Id : ", curr_tweet
+
+	except Exception as e :
+		print e
+	return features					
+
+def getFeatures(tweetFile, friendListFile, w_score, cleanTweetFile, tweetDocTopicFile, UserTweetLinks, UserRetweetLinks) :
 	tweets = {}
 	#Change the value of w in g(t - t')
 	weight_factor = float(w_score)
 	cleanTweets = set()
-	in_clean_tweets_file = open(cleenTweetFile, 'r')
+	in_clean_tweets_file = open(cleanTweetFile, 'r')
 	dataCleanTweets = in_clean_tweets_file.read()
 	lines = dataCleanTweets.split('\n')
 	for line in lines :
---
+++ src/scripts/recommend_mention_v2.py
@@ -0,0 +1,168 @@
+import pickle
+import sys
+import re
+import lda
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+from extractFeatures import *
+from sklearn.externals import joblib
+
+data = {}
+with open('../../data/algeria/UserCoeff.pickle', 'rb') as handle:
+  	data = pickle.load(handle)
+
+def getCoeffs(dict) :
+	dicts = {}
+	user_ids = []
+	for key in dict :
+		dicts[int(key)] = dict[key]['coeff']
+		user_ids.append(int(key))
+	print len(dicts)
+	return (dicts, user_ids)	
+
+def recommend_mention(tweet): 
+	#cleanTweets = []
+	#cleanTweetIds = []
+	predictCleanTweets = []
+	predictCleanTweets.append(tweet)
+	#predictCleanTweetIds = []
+    	topic_word = []
+	with open(sys.argv[1], 'r') as f:
+		for line in f:
+			try:
+				tweetParts = line.strip('\n').strip().split(' ', 1)
+				tweetId = tweetParts[0]
+				tweetprob = [float(x) for x in tweetParts[1:]]
+				topic_word.append(tweetprob)
+				#cleanTweets.append(tweetText)
+				#cleanTweetIds.append(tweetId)
+			except Exception as e:
+				#continue
+				exit()
+	
+	#vectorizer = CountVectorizer(analyzer = "word",   \
+        #                     tokenizer = None,    \
+        #                     preprocessor = None, \
+        #                     stop_words = None,   \
+        #                     max_features = 300) 
+
+	# fit_transform() does two functions: First, it fits the model
+	# and learns the vocabulary; second, it transforms our training data
+	# into feature vectors. The input to fit_transform should be a list of 
+	# strings.
+	#tweetDataFeatures = vectorizer.fit_transform(cleanTweets)
+
+	# Numpy arrays are easy to work with, so convert the result to an 
+	# array
+	#tweetDataFeatures = tweetDataFeatures.toarray()
+	#print tweetDataFeatures.shape
+	# outFile = open(sys.argv[2], 'w') 
+	# outFile.close()
+	vectorizer = joblib.load(sys.argv[2])
+	#vocab = vectorizer.get_feature_names()
+
+	#loglikelihood = []
+	#for numTopics in np.arange(5, 10, 5):
+	#	model = lda.LDA(n_topics=numTopics, n_iter=100, random_state=1)
+	#	model.fit(tweetDataFeatures)  # model.fit_transform(X) is also available
+	#	loglikelihood.append(model.loglikelihood())
+
+
+	
+	#numTopics = 100 #5 + 5*np.argmax(loglikelihood) 
+	#model = lda.LDA(n_topics=numTopics, n_iter=20, random_state=1)
+	#model.fit(tweetDataFeatures)  # model.fit_transform(X) is also available
+	#loglikelihood.append(model.loglikelihood())
+	#docTopicFile = open(sys.argv[2], 'w')
+	#docTopic = model.doc_topic_
+	#for i in xrange(0,len(cleanTweets)):
+	#	out = str(cleanTweetIds[i])
+	#	for j in docTopic[i]:
+	#		out += ' ' + str(j)
+	#	docTopicFile.write(out + '\n')
+	#docTopicFile.close()	
+
+	#termTopicFile = open(sys.argv[3], 'w')
+	#topic_word = model.topic_word_  # model.components_ also works
+	#n_top_words = int(sys.argv[4])
+	#for i, topic_dist in enumerate(topic_word):
+	#	topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
+	#	topic_words_prob = np.array(np.sort(topic_dist)[:-n_top_words:-1])
+	#	out = str(i)
+	#	for k in xrange(0,len(topic_words)):
+	#		out += '#' + topic_words[k] + ' ' + str(topic_words_prob[k])
+	#	termTopicFile.write(out + '\n')
+	#termTopicFile.close()
+	
+	# with open(sys.argv[3], 'r') as f:
+	# 	for line in f:
+	# 		try:
+	# 			tweetParts = line.strip('\n').strip().split(' ', 1)
+	# 			tweetId = tweetParts[0]
+	# 			tweetText = tweetParts[1]
+	# 			predictCleanTweets.append(tweetText)
+	# 			predictCleanTweetIds.append(tweetId)
+	# 		except Exception as e:
+	# 			continue
+
+	tweetPredictDataFeatures = vectorizer.transform(predictCleanTweets)
+	tweetPredictDataFeatures = tweetPredictDataFeatures.toarray()
+	print tweetPredictDataFeatures.shape
+	print tweetPredictDataFeatures[0]
+	#a = model.predict(tweetPredictDataFeatures)
+	#print a
+	# f = open(sys.argv[4], 'w')
+	topic_pred = []	
+	for i in xrange(0, len(predictCleanTweets)):
+		cumsum = 0.0
+		for topic_dist in topic_word:
+			sum = 0.0
+			for p in xrange(0,len(topic_dist)):
+				sum += topic_dist[p]*tweetPredictDataFeatures[i][p]	
+			#sumArray = topic_dist*tweetPredictDataFeatures[i]
+			#sum = 0.0
+			#for j in sumArray:
+			#	sum += j
+			cumsum += sum
+			topic_pred.append(sum)
+			#print sum
+		#print topic_pred
+		#print cumsum
+		for i in xrange(0, len(topic_pred)):
+			topic_pred[i] /= cumsum
+		#print topic_pred 		
+		#break
+		# tempDict = {}
+		# tempDict[int(predictCleanTweetIds[i])] = topic_pred
+		# f.write(str(tempDict) + '\n')
+		break	
+	# f.close()
+
+	recommend = {}
+	tweetTopicScores = getTweetScores("../../data/algeria/TweetDocTopic.txt")
+	print "Topic Scores Retrieved"
+	cleanTweets = getCleanTweets('../../data/algeria/CleanTweets.txt')
+	tweets = getTweets(cleanTweets, '../../data/algeria/ValidTweets.txt')
+	print "Tweets extracted"
+	user_friends = getFriends('../friendList_main.txt')
+	print "Friends extracted"
+	users = generateUserTweetMapping(cleanTweets, '../../data/algeria/ValidTweets.txt')
+	print "User Tweet mapping created"
+	(coeffs, user_ids) = getCoeffs(data)
+
+	for user_id in user_ids:
+		features = getTweetFeatures(user_id, tweetTopicScores, tweets, users, weight_factor, user_friends, topic_pred)
+		coeff = coeffs[user_id]
+		prod = features*coeff
+		_sum = 0
+		for i in prod:
+			_sum += i
+		prob = 1.0/(1+math.exp(-_sum))
+		recommend[user_id] = prob
+
+	return recommend
+
+
+
+if __name__ == '__main__':
+	main()
