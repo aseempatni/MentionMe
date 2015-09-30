0de3548b3efac9c205d492d4022aca2005f56541
Aseem Patni <apatnip@gmail.com>
2015-09-30 18:44:39 +0530

    updated to show sample tweets and loading image.

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+     $('#image').hide();
     $("#getreco").click(function(){
         tweet = $("#tweet").val();
         ip = "http://10.5.30.132:8080/";
@@ -8,30 +9,40 @@ $(document).ready(function(){
         data:{"tweet":tweet,
         "url":ip},
         headers:{"Accept": "application/json"},
+   	 beforeSend: function(){
+   	     $('#image').show();
+   	 },
+   	 complete: function(){
+   	     $('#image').hide();
+   	 },
         success: function(result){
             result = (result);
             x = "<ul>";
             recommendations = result["message"]["recommendations"]
             for (i in recommendations) {
+		user = recommendations[i];
                 if (user["name"] !="error") {
-                    user = recommendations[i];
                     x+="<li><a href = 'https://twitter.com/"+user["name"].substring(1)+"'>"+ user["name"] +"</a> ("+user["id"]+")</li>";
                 }
+		else {
+		    x+="<li>"+user["id"]+"</li>";
+		}
             }
             x+="</ul>";
             $("#recommendations").html(x);
         }});
     });
+
 });
 
 function get_user_name_from(user_id) {
 	var url = "http://tweeterid.com/ajax.php"
 	$.ajax({
-	url: url,
-	data: {input:user_id},
-	type: 'post',
-	success: function (response) {//response is value returned from php
-		console.log(response)
-	}
-});
+    	url: url,
+    	data: {input:user_id},
+    	type: 'post',
+    	success: function (response) {//response is value returned from php
+    		console.log(response)
+    	}
+    });
 }
